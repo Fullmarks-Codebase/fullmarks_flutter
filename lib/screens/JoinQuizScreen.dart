e9ec735cb6b27224a1bc6965a0c5b9c6db8eea22
Ankit <jaimin@webbrainstechnologies.com>
2020-12-22 18:19:25 +0530

    join quiz

---
+++ lib/screens/JoinQuizScreen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fullmarks/utility/appAssets.dart';
+import 'package:fullmarks/utility/appColors.dart';
+import 'package:fullmarks/utility/utiity.dart';
+
+class JoinQuizScreen extends StatefulWidget {
+  @override
+  _JoinQuizScreenState createState() => _JoinQuizScreenState();
+}
+
+class _JoinQuizScreenState extends State<JoinQuizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Utility.setSvgFullScreen(context, AppAssets.mockTestBg),
+          Column(
+            children: [
+              Spacer(),
+              SvgPicture.asset(
+                AppAssets.bottomBarbg,
+                width: MediaQuery.of(context).size.width,
+              )
+            ],
+          ),
+          body(),
+        ],
+      ),
+    );
+  }
+
+  Widget body() {
+    return Column(
+      children: [
+        Utility.appbar(
+          context,
+          text: "",
+          onBackPressed: () {
+            Navigator.pop(context);
+          },
+          isHome: false,
+        ),
+        Spacer(),
+        Text(
+          "Join Quiz",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(16),
+          padding: EdgeInsets.all(16),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Text(
+            "139829",
+            style: TextStyle(
+              color: AppColors.appColor,
+              fontSize: 30,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Utility.button(
+            context,
+            onPressed: null,
+            text: "Enter",
+            bgColor: AppColors.myProgressCorrectcolor,
+            isSufix: true,
+            assetName: AppAssets.enter,
+            isSpacer: true,
+          ),
+        ),
+        Spacer(),
+        Spacer(),
+      ],
+    );
+  }
+}
